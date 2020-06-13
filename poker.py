9a2683847130629e9dcc451642d5126a46202722
Lyell Read <lread@silverstar>
2020-06-12 20:42:50 -0700

    Defining gamemodes at top, and neatening function of poker.py

--- poker.py
+++ poker.py
@@ -4,6 +4,21 @@ from player_class import *
 from hand_class import *
 from cards_deck import *
 
+games = ["Baseball", 
+            "Queens", 
+            "Whores", 
+            "Nicks", 
+            "Texas", 
+            "Omaha", 
+            "test", 
+            "0/54", 
+            "7_card_screw", 
+            "Elevator", 
+            "1_card_screw", 
+            "D_and_G", 
+            "Kings", 
+            "7/27"]
+
 def main():
     starting_chip_stack = int(input("Enter starting chip stack amount: "))
     auntie = int(input("Enter auntie amount for games: "))
@@ -18,15 +33,17 @@ def main():
     players = []
     player_info = open('player_info.txt', 'w')
     player_info.close()
-    
+    # players.append(Player("Noah"))
+    # players.append(Player("Tova"))
+    # players.append(Player("Alex"))
+    # players.append(Player("Steve"))
     while(1==1):
         player_name = input("Enter player name (type done to move on):  ")
         if player_name == "done":
             break
         players.append(Player(player_name, starting_chip_stack))
 
-    dealer_i = 0
-    
+    dealer_i =  0
     while(1==1):
         print()
         print(f"New game, {players[dealer_i].name} is dealer")
@@ -35,17 +52,16 @@ def main():
             print(player)
 
         print("What game should we play? Type 'game over' to end, 'remove player' to remove a player, 'add player' to add a player.")
-        gametype = input("Game Options: Baseball, Nicks, Queens, Whores, Texas, Omaha, 0/54, 7_card_screw, Elevator, 1_card_screw, D_and_G, Kings, 7/27:  ")
-        games = ["Baseball", "Queens", "Whores", "Nicks", "Texas", "Omaha", "test", "0/54", "7_card_screw", "Elevator", "1_card_screw", "D_and_G", "Kings", "7/27"]
+        gametype = input("Game Options:", ' '.join(games), ": ")
+        
 
         if gametype == "game over":
             break
 
         elif gametype == "remove player":
             name = input("What is the name of the player to remove?  ")
-            for player in players:
-                if player.name == name:
-                    players.remove(player)
+            if player in players:
+                players.remove(player)
 
         elif gametype == "add player":
             name = input("Enter plyer name:  ")
@@ -59,11 +75,8 @@ def main():
             game = Game(gametype, players, dealer_i, auntie, card_color)
             game.play()
 
-            dealer_i += 1
-
-            #loop the dealer
-            if dealer_i > len(players)-1:
-                dealer_i = 0
+            # increment dealer, loop if needed
+            dealer_i = (dealer_i + 1) % (len(players))
 
         #reset players hands and bet amount
         for player in players:
